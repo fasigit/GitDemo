42f044403aba846af7c424ff73fb73ac2ec7c8ec
Faisal <qaqccheck111@gmail.com>
2020-11-18 12:22:27 -0500

    first commit

---
+++ src/test/java/learningmaven/Mavenjava/Testcase1.java
@@ -0,0 +1,14 @@
+package learningmaven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	
+	@Test
+	public void Yahoopage()
+	{
+		System.out.println("I am Yahoo");
+	}
+	
+
+}
---
+++ src/test/java/learningmaven/Mavenjava/Testcase2.java
@@ -0,0 +1,19 @@
+package learningmaven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+	
+@Test
+	public void googlepage()
+	{
+		System.out.println("I am TC1");
+	}
+	
+	@Test
+	public void googlepagedisplays()
+	
+	{
+		System.out.println("Google page displays");
+	}
+}
